eaad2a7f3e0680349b9be4d046a4bfae2daa5fb6
evan-zl <evan2016@vip.qq.com>
2024-09-26 11:14:37 +0800

    fix:辅助交叉点

--- hooks/grid.ts
+++ hooks/grid.ts
@@ -1,41 +1,46 @@
-import * as THREE from 'three'
+import * as THREE from "three";
 
-import type { Grid } from '../types'
-type Options = Pick<Grid, 'width' | 'divisions' | 'forkColor' | 'forkSize'>
+import type { Grid } from "../types";
+type Options = Pick<Grid, "width" | "divisions" | "forkColor" | "forkSize">;
 
-export declare type Params = import('../types/utils').DeepPartial<Options>
+export declare type Params = import("../types/utils").DeepPartial<Options>;
 
 // 网格交叉
 export const useGrid = () => {
   const createFork = (options: Params = {}) => {
-    const { width = 800, divisions = 80, forkSize = 1.4, forkColor = 0xa1a1a1 } = options
+    const {
+      width = 800,
+      divisions = 80,
+      forkSize = 1.4,
+      forkColor = 0xa1a1a1,
+    } = options;
     let step = width / divisions,
-      start = -width / 2
-    const group = new THREE.Group()
+      start = -width / 2;
+    const group = new THREE.Group();
     for (let i = 0; i <= divisions; i++) {
       for (let j = 0; j <= divisions; j++) {
-        const x = start + i * step
-        const z = start + j * step
-        const geo = new THREE.PlaneGeometry(forkSize, forkSize / 5)
+        const x = start + i * step;
+        const z = start + j * step;
+        const geo = new THREE.PlaneGeometry(forkSize, forkSize / 5);
         // 边框材质
         const mat = new THREE.MeshLambertMaterial({
           color: forkColor,
           transparent: true,
-          opacity: 0.9
-        })
-        const mesh = new THREE.Mesh(geo, mat)
-        mesh.rotateX(-Math.PI * 0.5)
-        mesh.position.set(x, 0, z)
-        const mesh2 = mesh.clone()
-        mesh2.rotateZ(Math.PI * 0.5)
-        group.add(mesh, mesh2)
+          opacity: 0.9,
+        });
+        const mesh = new THREE.Mesh(geo, mat);
+        mesh.rotateX(-Math.PI * 0.5);
+        mesh.position.set(x, 0, z);
+        const mesh2 = mesh.clone();
+        mesh2.rotateZ(Math.PI * 0.5);
+        group.add(mesh, mesh2);
       }
     }
-    group.name = '辅助交点'
-    return group
-  }
+    group.name = "辅助交叉点";
+    return group;
+  };
 
   return {
-    createFork
-  }
-}
+    createFork,
+  };
+};
