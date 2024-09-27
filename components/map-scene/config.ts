be5c8dfb9d78370997ca5eba379f0e6f177863b6
evan-zl <evan2016@vip.qq.com>
2024-09-27 14:20:03 +0800

    add:增加 map 场景

---
+++ components/map-scene/config.ts
@@ -0,0 +1,28 @@
+export default {
+  // 地图深度
+  depth: 1,
+  // 地图缩放倍数
+  scale: 40,
+  // 右键间隔时间
+  rightClickBackDiffTime: 100,
+
+  // 区域 label
+  areaLabel: false,
+  // 光柱
+  markLight: false,
+  // 上下边框
+  border: true,
+  // 地图背景
+  mapBg: true,
+  // 地图柱状图
+  mapBar: true,
+
+  // 地图贴图
+  map: {
+    // map: '/oss/textures/map/gz-map.jpg',
+    // normal: '/oss/textures/map/gz-map-fx.jpg',
+    // side: '/oss/textures/map/border.png'
+    // bgOutCircle: '/oss/textures/map/out-circle.png'
+    // bgInnerCircle: '/oss/textures/map/inner-circle.png'
+  }
+}
