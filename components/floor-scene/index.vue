90d9cbbdd2d420da004a5358cdfb12d5641e4283
evan-zl <evan2016@vip.qq.com>
2024-09-30 16:04:19 +0800

    fix:手动引入 toRaw 方法

--- components/floor-scene/index.vue
+++ components/floor-scene/index.vue
@@ -33,7 +33,7 @@
 </template>
 
 <script lang="ts" setup>
-import { watch, ref, withDefaults, onMounted } from 'vue'
+import { watch, ref, toRaw, withDefaults, onMounted } from 'vue'
 import * as THREE from 'three'
 import * as TWEEN from 'three/examples/jsm/libs/tween.module.js'
 
