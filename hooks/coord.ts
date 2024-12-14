b4675cfa9697330f42077a4e7dce77692f6673be
evan-zl <evan2016@vip.qq.com>
2024-12-14 09:13:54 +0800

    fix:代码格式化

--- hooks/coord.ts
+++ hooks/coord.ts
@@ -5,11 +5,17 @@ export const useCoord = () => {
   const getBoundingBox = group => {
     // 包围盒计算模型对象的大小和位置
     var box3 = new THREE.Box3()
-    box3.expandByObject(group) // 计算模型包围盒
+
+    // 计算模型包围盒
+    box3.expandByObject(group)
     var size = new THREE.Vector3()
-    box3.getSize(size) // 计算包围盒尺寸
+
+    // 计算包围盒尺寸
+    box3.getSize(size)
     var center = new THREE.Vector3()
-    box3.getCenter(center) // 计算一个层级模型对应包围盒的几何体中心坐标
+
+    // 计算一个层级模型对应包围盒的几何体中心坐标
+    box3.getCenter(center)
     return {
       box3,
       center,
