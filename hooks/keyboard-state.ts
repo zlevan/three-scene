e302a86373fca2d1a9b04bc69096ba3663b5f43a
evan-zl <evan2016@vip.qq.com>
2024-12-19 11:29:17 +0800

    fix:优化

--- hooks/keyboard-state.ts
+++ hooks/keyboard-state.ts
@@ -17,6 +17,10 @@ export const useKeyboardState = () => {
   const _keyCodes = {}
   const _modifiers = {}
 
+  // 插入回调
+  let _onKeydownCall: (event) => void
+  let _onKeyupCall: (event) => void
+
   const _onKeyChange = (event, pressed) => {
     var keyCode = event.keyCode
     _keyCodes[keyCode] = pressed
@@ -30,10 +34,18 @@ export const useKeyboardState = () => {
 
   const _onKeyDown = event => {
     _onKeyChange(event, true)
+    if (typeof _onKeydownCall === 'function') _onKeydownCall(event)
   }
 
   const _onKeyUp = event => {
     _onKeyChange(event, false)
+    if (typeof _onKeyupCall === 'function') _onKeyupCall(event)
+  }
+
+  // 事件插入
+  const insertEvent = (onKeydown, onKeyup) => {
+    _onKeydownCall = onKeydown
+    _onKeyupCall = onKeyup
   }
 
   // 绑定事件
@@ -41,14 +53,17 @@ export const useKeyboardState = () => {
   document.addEventListener('keyup', _onKeyUp, false)
 
   // 检测按下的按键(可组合键 alt+W)
-  const pressed = keyDesc => {
+  const detection = (keyDesc: string) => {
     var keys = keyDesc.split('+')
     for (var i = 0; i < keys.length; i++) {
       var key = keys[i]
       var pressed
+      // 组合按键
       if (MODIFIERS.indexOf(key) !== -1) {
         pressed = _modifiers[key]
-      } else if (Object.keys(ALIAS).indexOf(key) != -1) {
+      }
+      // 方向
+      else if (Object.keys(ALIAS).indexOf(key) != -1) {
         pressed = _keyCodes[ALIAS[key]]
       } else {
         pressed = _keyCodes[key.toUpperCase().charCodeAt(0)]
@@ -58,13 +73,21 @@ export const useKeyboardState = () => {
     return true
   }
 
+  const keyboardPressed = (keys: string | string[]) => {
+    if (Array.isArray(keys)) {
+      return keys.findIndex(detection) > -1
+    }
+    return detection(keys)
+  }
+
   const destroyEvent = () => {
     document.removeEventListener('keydown', _onKeyDown, false)
     document.removeEventListener('keyup', _onKeyUp, false)
   }
 
   return {
-    pressed,
+    keyboardPressed,
+    insertEvent,
     destroyEvent
   }
 }
